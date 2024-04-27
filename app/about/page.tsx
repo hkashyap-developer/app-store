44a74df8693b4f10b0cdeeb3d17d0fa312a9f3d9
Himanshu Kashyap <87484630+hkashyap-developer@users.noreply.github.com>
2024-04-28 03:27:07 +0530

    shadecn
    shadecn

---
+++ app/about/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Toaster } from "@/components/ui/sonner"
+
+function page() {
+    return (
+        <>
+
+
+
+            test1
+            <Toaster />
+            test2
+
+        </>
+    )
+}
+
+export default page
