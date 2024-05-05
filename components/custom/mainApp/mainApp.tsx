6e67cb3e4d01a9baba6944b310170e36faa8d2ec
Himanshu Kashyap <87484630+hkashyap-developer@users.noreply.github.com>
2024-05-05 10:31:15 +0530

    committ
    committ

--- components/custom/mainApp/mainApp.tsx
+++ components/custom/mainApp/mainApp.tsx
@@ -6,14 +6,24 @@ import { Button } from "@/components/ui/button"
 import { ToastAction } from "@/components/ui/toast"
 import { useToast } from "@/components/ui/use-toast"
  
+import { Calendar } from "@/components/ui/calendar"
 
 
 const mainApp = () => {
+
+  const [date, setDate] = React.useState<Date | undefined>(new Date())
 
   const { toast } = useToast();
 
   return (
     <>
+      <Calendar
+    mode="single"
+    selected={date}
+    onSelect={setDate}
+    className="rounded-md border"
+  />
+
     <h2>Main App Content</h2>
     </>
   )
